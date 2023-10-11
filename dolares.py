7c5c5e1639a42eec68b9c10e900b435087acb5ac
isaacsaless <isaacnascimentosales@gmail.com>
2023-10-11 11:15:54 -0300

    Primeira versão do código de conversão de dolares

---
+++ dolares.py
@@ -0,0 +1,6 @@
+# Digite um valor e veja quantos dolares voce poderá comprar: R$
+print('Conversor de reias para dolares')
+
+real = float(input('Digite um valor em reais(R$): '))
+dolar = real*5
+print(f'Convertendo {real} reais você terá {dolar} dolares.')
